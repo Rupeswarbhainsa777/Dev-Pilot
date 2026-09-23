02475d8a1c7359427fd4fe073e5656f7fe62e0fb
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-23 21:56:05 +0530

    App js updated

--- devpilot/src/App.jsx
+++ devpilot/src/App.jsx
@@ -2,16 +2,20 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import NaveBar from "./components/NaveBar/NaveBar.jsx";
 
 function App() {
     return (
 
 
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
+        <>
+            <NaveBar />
+            <Routes>
 
-        </Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+
+            </Routes></>
     )
 }
 
