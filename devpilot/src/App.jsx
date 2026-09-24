c6123b7361e44515b8fb16766b5138de870dc3fe
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-24 22:56:56 +0530

    fixed app

--- devpilot/src/App.jsx
+++ devpilot/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/history" element={<History/>}/>
-                <Route path="/about" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
 
             </Routes></>
     )
