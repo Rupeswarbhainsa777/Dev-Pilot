925fdb56d6680c5552661c60b24533141ad7af2b
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-26 21:54:09 +0530

    Working auth

---
+++ Backend/src/main/java/devPilot/Backend/controller/AuthController.java
@@ -0,0 +1,13 @@
+package devPilot.Backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth/api")
+public class AuthController {
+
+
+
+}
